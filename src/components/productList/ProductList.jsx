11811460f4b246a71ba66deb2dcd2b750912ee80
Joshua Litam <joshua_litam@yahoo.com>
2023-06-16 16:44:26 +0800

    added frameworks

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -8,7 +8,7 @@ const ProductList = () => {
     <div className="pl">
         <div className="pl-texts">
             <h1 className="pl-title">Previous Works</h1>
-            <p className="pl-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempor, nibh quis venenatis vehicula, ex ex maximus elit, at sagittis justo libero ac nisl. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam ornare sem lacus, et mollis odio sollicitudin a</p>
+            <p className="pl-desc">Commonly used Frameworks: Mongodb, React, MUI, Bootstrap, Tailwind</p>
         </div>
         <div className="pl-list">
             {products.map(product => (
