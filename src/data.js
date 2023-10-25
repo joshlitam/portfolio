a4d2c59744df3a5bbf601c29f91854333926d044
Joshua Litam <joshua_litam@yahoo.com>
2023-10-25 09:22:09 +0800

    added new project

--- src/data.js
+++ src/data.js
@@ -1,17 +1,17 @@
 export const products = [
-    {
-      id: 1,
-      img: "https://user-images.githubusercontent.com/74515811/246002238-41df847b-c08b-4fa5-a17d-5de97bcaa6cf.png",
-      link: "https://github.com/joshlitam/etch-a-sketch",
-    },
-    {
-      id: 2,
-      img: "https://github-production-user-asset-6210df.s3.amazonaws.com/74515811/247041562-4393dab4-4fa0-428e-b39d-c8df1132d28a.png",
-      link: "https://sky-hotel-test-2.onrender.com/",
-    },
-    {
-        id: 4,
-        img: "https://github-production-user-asset-6210df.s3.amazonaws.com/74515811/246325324-0354413c-0d38-420f-ac18-806118b11497.png",
-        link: "https://joshlitam.github.io/anime-waifu-pic/",
-        },
-  ];
+  {
+    id: 1,
+    img: "https://raw.githubusercontent.com/joshlitam/Big-Joe-Inc/main/public/img/bigjoeinc.png",
+    link: "https://github.com/joshlitam/Big-Joe-Inc",
+  },
+  {
+    id: 2,
+    img: "https://github-production-user-asset-6210df.s3.amazonaws.com/74515811/247041562-4393dab4-4fa0-428e-b39d-c8df1132d28a.png",
+    link: "https://sky-hotel-test-2.onrender.com/",
+  },
+  {
+    id: 4,
+    img: "https://github-production-user-asset-6210df.s3.amazonaws.com/74515811/246325324-0354413c-0d38-420f-ac18-806118b11497.png",
+    link: "https://joshlitam.github.io/anime-waifu-pic/",
+  },
+];
